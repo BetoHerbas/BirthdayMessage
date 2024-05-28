ce780cce6778f7ed2952828b185923bbb449700a
Beto Herbas <betitoherbas@gmail.com>
2024-05-28 01:18:33 -0400

    Refactoring: Extracting sendEmail to another function

--- src/birthday/BirthdayService.js
+++ src/birthday/BirthdayService.js
@@ -6,15 +6,18 @@ export class BirthdayService {
   sendGreetings(ourDate, smtpUrl, smtpPort, transport) {
     let employees = this.employeesRepository.getEmployeesByBirthDate(ourDate);
     employees.forEach((employee) => {
-      const message = {
-        host: smtpUrl,
-        port: smtpPort,
-        from: "sender@here.com",
-        to: [employee.getEmail()],
-        subject: "Happy Birthday!",
-        text: `Happy Birthday, dear ${employee.getFirstName()}!`,
-      };
-      transport.sendMail(message);
     });
+  }
+
+  seendGreatingToEmployee(employee, smtpUrl, smtpPort, transport) {
+    const message = {
+      host: smtpUrl,
+      port: smtpPort,
+      from: "sender@here.com",
+      to: [employee.getEmail()],
+      subject: "Happy Birthday!",
+      text: `Happy Birthday, dear ${employee.getFirstName()}!`,
+    };
+    transport.sendMail(message);
   }
 }
